90055f2c25b42dc2d4b7963acccdf3dcf1e5390b
Marlen Mazinov <maraton.bashirov@yandex.ru>
2022-10-12 19:15:18 +0300

    1. ToMany [#301848]

--- src/main/java/ru/job4j/cars/model/Post.java
+++ src/main/java/ru/job4j/cars/model/Post.java
@@ -1,21 +1,28 @@
 package ru.job4j.cars.model;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 import java.util.Date;
+import java.util.List;
 
 
 @Entity
 @Table(name = "post")
 @Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private int id;
     private String description;
     private Date created;
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "prices")
+    private List<PriceHistory> prices;
 }
